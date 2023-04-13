38a2e190de6c0a096040a07bc3247a8bb4440877
lakshmilovely <105770396+lakshmilovely@users.noreply.github.com>
2023-04-13 12:41:17 +0530

    Add files via upload

---
+++ environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  apiUrl:"https://devtaskapi.axelautomotive.com/api/",
+  thumbUrl:"https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/thumbs/",
+  imgURL:"https://devaxeloneapi.axelautomotive.com/api/v1/resources/profilepics/",
+  traxapiUrl:"https://devtraxapi.axelautomotive.com/api/"
+};
